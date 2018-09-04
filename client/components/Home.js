d6a4e6547761f191ee70e768b06e76012a3a5286
isaac1104 <isaac-kwon@hotmail.com>
2018-09-03 17:23:15 -0700

    style buttons

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 
 const Home = () => {
   return (
     <View>
-      <Text>Kitchen View</Text>
-      <Text>Customer View</Text>
-      <Text>Setting</Text>
+      <TouchableOpacity>
+        <Text style={styles.button}>Kitchen View</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.button}>Customer View</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.button}>Setting</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#4ecdc4',
+    color: 'white',
+    height: 60,
+    lineHeight: 60,
+    marginTop: 10,
+    textAlign: 'center',
+    width: 300
+  },
+});
 
 export default Home;
