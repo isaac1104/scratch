3131518d906ac7bfcfec85552192a9ded733ad4f
isaac1104 <isaac-kwon@hotmail.com>
2018-09-04 19:56:57 -0700

    add placeholders inside settings route

--- src/client/components/Setting.js
+++ src/client/components/Setting.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import { Text, View } from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
 
 const Setting = () => {
   return (
     <View>
-      <Text>Setting</Text>
+      <TouchableOpacity>
+        <Text style={styles.button}>Update Account Info</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.button}>Add Menu</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.button}>View Orders</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.button}>Log Out</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#4ecdc4',
+    color: 'white',
+    height: 60,
+    lineHeight: 60,
+    marginTop: 10,
+    textAlign: 'center',
+    width: 300
+  },
+});
 
 export default Setting;
