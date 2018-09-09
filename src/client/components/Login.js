1941cfe593abc3e29fab379575e7d0d778e76a14
isaac1104 <isaac-kwon@hotmail.com>
2018-09-08 20:39:28 -0700

    refactor login.js

--- src/client/components/Login.js
+++ src/client/components/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import LoginForm from './Form/LoginForm';
 
 class Login extends Component {
