a9013224b8cbc7cdc6b5d38cd742c0f334b06240
isaac1104 <isaac-kwon@hotmail.com>
2018-09-15 19:53:45 -0700

    create action creator to save uuid

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -4,3 +4,8 @@ export const saveTableNumber = number => ({
   type: types.SAVE_TABLE_NUMBER,
   payload: number
 });
+
+export const saveDeviceUUID = uuid => ({
+  type: types.SAVE_DEVICE_UUID,
+  payload: uuid
+});
