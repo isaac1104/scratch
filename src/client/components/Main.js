dee14a94381cbdcf5220b57fd692ca099799718f
Isaac Kwon <isaac-kwon@hotmail.com>
2018-09-17 15:15:07 -0700

    refactor flex box

--- src/client/components/Main.js
+++ src/client/components/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { NativeRouter, Route, Switch } from 'react-router-native';
 import Navbar from './Navbar';
@@ -12,7 +12,7 @@ class Main extends Component {
   render() {
     return (
       <NativeRouter>
-        <View style={styles.container}>
+        <Fragment>
           <Navbar />
           <Switch>
             <Route exact path='/' component={Login} />
@@ -21,19 +21,10 @@ class Main extends Component {
             <Route exact path='/customer' component={CustomerView} />
             <Route exact path='/setting' component={Setting} />
           </Switch>
-        </View>
+        </Fragment>
       </NativeRouter>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default Main;
