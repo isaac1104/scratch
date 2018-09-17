271c187f2042efed773022da5084bf85254b4a43
isaac1104 <isaac-kwon@hotmail.com>
2018-09-16 21:07:35 -0700

    create action creators for login

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as types from './types';
 
 export const saveTableNumber = number => ({
@@ -9,3 +10,25 @@ export const saveDeviceUUID = uuid => ({
   type: types.SAVE_DEVICE_UUID,
   payload: uuid
 });
+
+const userAuthRequest = () => ({
+  type: types.USER_AUTH_REQUEST,
+  payload: true
+});
+
+const userAuthSuccess = token => ({
+  type: types.USER_AUTH_SUCCESS,
+  payload: token
+});
+
+const userAuthFail = error => ({
+  type: types.USER_AUTH_FAIL,
+  payload: error
+});
+
+export const signin = data => async dispatch => {
+  dispatch(userAuthRequest());
+  const request = await axios.post('http://159.89.143.187/api/login', data);
+  const { data } = request;
+  console.log(data);
+};
