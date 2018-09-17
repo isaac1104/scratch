042dc4ef6c3d94229570f284d8ccfe16dd018b91
isaac1104 <isaac-kwon@hotmail.com>
2018-09-16 21:10:53 -0700

    add user reducer

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import tableNumberReducer from './table_number_reducer';
 import deviceUuidReducer from './device_uuid_reducer';
+import userAuthReducer from './user_auth_reducer';
 
 const rootReducer = combineReducers({
   tableNumber: tableNumberReducer,
   device_uuid: deviceUuidReducer,
+  user: userAuthReducer,
   form: formReducer
 });
 
---
+++ src/client/reducers/user_auth_reducer.js
@@ -0,0 +1,35 @@
+import { USER_AUTH_REQUEST, USER_AUTH_SUCCESS, USER_AUTH_FAIL } from '../actions';
+
+const INITIAL_STATE = {
+  isAuthenticating: false,
+  token: '',
+  errorMsg: ''
+};
+
+function userAuthReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case USER_AUTH_REQUEST:
+      return {
+        ...state,
+        isAuthenticating: action.payload
+      };
+    case USER_AUTH_SUCCESS:
+      return {
+        ...state,
+        isAuthenticating: false,
+        token: action.payload,
+        errorMsg: ''
+      };
+    case USER_AUTH_FAIL:
+      return {
+        ...state,
+        isAuthenticating: false,
+        token: '',
+        errorMsg: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default userAuthReducer;
