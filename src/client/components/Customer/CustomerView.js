8a6fba7842bf88ccbf2ab07e41d010d946f1ba4b
isaac1104 <isaac-kwon@hotmail.com>
2018-09-17 20:04:00 -0700

    add button to control sidebar

--- src/client/components/Customer/CustomerView.js
+++ src/client/components/Customer/CustomerView.js
@@ -12,17 +12,32 @@ class CustomerView extends Component {
   render() {
     const style = {
       sidebar: {
-        display: 'flex',
+        position: 'absolute',
+        right: 0,
+        top: 0,
+        bottom: 0,
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         width: this.state.isOpen ? '20%' : 0,
         backgroundColor: '#eeeeee'
+      },
+      button: {
+        position: 'absolute',
+        right: 0,
+        marginRight: this.state.isOpen ? '20%' : 0
       }
     };
 
     return (
       <View style={styles.container}>
         <Menus />
+        <Button
+          onPress={() => this.setState({ isOpen: !this.state.isOpen })}
+          style={style.button}
+        >
+          {this.state.isOpen ? 'Close' : 'Open'}
+        </Button>
         <View style={style.sidebar}>
           <Text>Sidebar</Text>
         </View>
