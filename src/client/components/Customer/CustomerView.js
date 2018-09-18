529cfdfd96f092567eee5843fb8ab6f294534cb8
isaac1104 <isaac-kwon@hotmail.com>
2018-09-17 19:53:01 -0700

    create sidebar

--- src/client/components/Customer/CustomerView.js
+++ src/client/components/Customer/CustomerView.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import Menus from './Menus';
 import { connect } from 'react-redux';
+import { Button } from 'react-native-paper';
 
 class CustomerView extends Component {
+  state = {
+    isOpen: false
+  };
+
   render() {
+    const style = {
+      sidebar: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: this.state.isOpen ? '20%' : 0,
+        backgroundColor: '#eeeeee'
+      }
+    };
+
     return (
       <View style={styles.container}>
         <Menus />
+        <View style={style.sidebar}>
+          <Text>Sidebar</Text>
+        </View>
       </View>
     );
   }
