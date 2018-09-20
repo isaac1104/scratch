c65755c3ad47ef28b3ce3b15c0711c130304d5f6
Isaac Kwon <isaac-kwon@hotmail.com>
2018-09-20 14:22:45 -0700

    refactor

--- src/client/components/Login.js
+++ src/client/components/Login.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { withRouter } from 'react-router-native';
 import LoginForm from './Form/LoginForm';
-import Expo from 'expo';
+import { Constants, SecureStore } from 'expo';
 import { connect } from 'react-redux';
 import { saveDeviceUUID } from '../actions';
 
 class Login extends Component {
   componentDidMount() {
-    const UUID = Expo.Constants.deviceId;
+    const UUID = Constants.deviceId;
     this.props.saveDeviceUUID(UUID);
     this.checkIfLoggedIn();
   };
 
   async checkIfLoggedIn() {
-    const token = await Expo.SecureStore.getItemAsync('token');
+    const token = await SecureStore.getItemAsync('token');
     if (token) {
       this.props.history.push('/home');
     }
