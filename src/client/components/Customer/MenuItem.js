7200d403a362b0fa3d5e3faa295d9bb5a8d94eed
Isaac Kwon <isaac-kwon@hotmail.com>
2018-09-21 14:03:24 -0700

    add more boxes for full list

--- src/client/components/Customer/MenuItem.js
+++ src/client/components/Customer/MenuItem.js
@@ -4,9 +4,26 @@ import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 class MenuItem extends Component {
   render() {
     return (
-      <TouchableOpacity style={styles.button}>
-        <Text>{this.props.item}</Text>
-      </TouchableOpacity>
+      <Fragment>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>{this.props.item}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>{this.props.item}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>{this.props.item}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>{this.props.item}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>{this.props.item}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>{this.props.item}</Text>
+        </TouchableOpacity>
+      </Fragment>
     );
   }
 }
@@ -15,7 +32,13 @@ const styles = StyleSheet.create({
   button: {
     width: 300,
     height: 50,
-    backgroundColor: '#ffffff'
+    backgroundColor: '#2f3136',
+    marginBottom: 10
+  },
+  text: {
+    textAlign: 'right',
+    lineHeight: 50,
+    color: '#ffffff'
   }
 });
 
