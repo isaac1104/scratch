dd723bbdaa0acad14e7581b759c8aa67f15ce9c9
Isaac Kwon <isaac-kwon@hotmail.com>
2018-09-24 11:17:25 -0700

    change TouchableOpacity to Card

--- src/client/components/Customer/MenuItem.js
+++ src/client/components/Customer/MenuItem.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { Text, StyleSheet, TouchableOpacity, Image, ActivityIndicator } from 'react-native';
+import { Card, Title } from 'react-native-paper';
 
 class MenuItem extends Component {
   state = {
@@ -8,17 +9,19 @@ class MenuItem extends Component {
 
   render() {
     return (
-      <TouchableOpacity style={styles.itemBox}>
-        <Text style={styles.text}>{this.props.item.toUpperCase()}</Text>
-        <Text style={styles.text}>Description</Text>
-        <Image
-          source={{ uri: this.props.image }}
+      <Card style={styles.itemBox} elevation={5}>
+        <Card.Content>
+          <Title style={styles.text}>{this.props.item.toUpperCase()}</Title>
+          <Text style={styles.text}>Description</Text>
+        </Card.Content>
+        <Card.Cover
           style={styles.image}
+          source={{ uri: this.props.image }}
           onLoadStart={() => this.setState({ loading: true })}
           onLoadEnd={() => this.setState({ loading: false })}
         />
         {this.state.loading && <ActivityIndicator size='large' style={styles.image} color='#222222' />}
-      </TouchableOpacity>
+      </Card>
     );
   }
 }
@@ -29,10 +32,7 @@ const styles = StyleSheet.create({
     height: 80,
     backgroundColor: '#ffffff',
     marginBottom: 10,
-    marginTop: 10,
-    shadowOffset: { width: 6,  height: 6 },
-    shadowColor: 'black',
-    shadowOpacity: 0.7
+    marginTop: 10
   },
   text: {
     textAlign: 'left',
@@ -43,7 +43,8 @@ const styles = StyleSheet.create({
     width: 100,
     height: 80,
     position: 'absolute',
-    right: 0
+    right: 0,
+    backgroundColor: '#ffffff'
   }
 });
 
